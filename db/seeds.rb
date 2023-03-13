544051110fa20fb196d26a6f7e2829c93f4ea31b
tony555t <tonytullah764@gmail.com>
2023-03-13 19:39:59 +0300

    active record lab

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.create!(first_name: 'toni', last_name: 'tula', grade: 100)
+Student.create!(first_name: ' Jack',last_name:'kim', grade: 50)
+Student.create!(first_name: 'Limburger', last_name: 'fredie', grade: 90)
